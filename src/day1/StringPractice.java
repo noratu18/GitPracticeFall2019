0338a42d9f2e40dd7b0f5066044f18e203dfc022
noratu18 <noratukesh@gmail.com>
2020-02-29 15:41:05 -0500

    added loop practice file

--- src/day1/StringPractice.java
+++ src/day1/StringPractice.java
@@ -3,11 +3,9 @@ package day1;
 public class StringPractice {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
-
-
-
+        String str = "java is fun!";
+        System.out.println("str = " + str);
+        
+        
     }
-
-
-
 }
