e017686048bcc42ce71d253211d488da5ecbf7e4
noratu18 <noratukesh@gmail.com>
2020-02-29 15:52:09 -0500

    update

--- src/LoopPractice.java
+++ src/LoopPractice.java
@@ -1,10 +1,11 @@
 public class LoopPractice {
     public static void main(String[] args) {
 
-
         // some random code
         for(int i =0; i<10; i+=2){
             System.out.println("i = " + i);
+            System.out.println("########");
+
         }
     }
 }
